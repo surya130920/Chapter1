930c948f4d79010371a2ad201fd7943feb4cea5a
unknown <surya.lakshmipathy@trumpf.com>
2025-01-02 16:27:55 +0530

    Ex_1.3: Initial commit

---
+++ Project1/Ex_1.3.c
@@ -0,0 +1,18 @@
+
+
+#include<stdio.h>
+
+main() {
+	int fahr, cels;
+	int lower, upper, step;
+	lower = 0;
+	upper = 300;
+	step = 20;
+	printf("Fahrenheit \tCelsius\n");
+	fahr = lower;
+	while (fahr <= upper) {
+		cels = 5 * (fahr - 32) / 9;
+		printf("%d\t\t%d\n", fahr, cels);
+		fahr = fahr + step;
+	}
+}
