876a354c9df250f3fcd4342d69c8b755267a2418
surya130920 <surya.lakshmipathy@trumpf.com>
2025-01-07 15:10:02 +0530

    Ex_1.3: Submission

---
+++ Project1/Exercise_1.3.c
@@ -0,0 +1,16 @@
+//1.3 Modify the temperature coversion program to print a heading above the table
+#include<stdio.h>
+
+main() {
+	int fahr, cels, lower, upper, step;
+	lower = 0;
+	upper = 300;
+	step = 20;
+	printf("Fahrenheit      Celsius\n");
+	fahr = lower;
+	while (fahr <= upper) {
+		cels = 5 * (fahr - 32) / 9;
+		printf("%3d               %3d\n", fahr, cels);
+		fahr = fahr + step;
+	}
+}
